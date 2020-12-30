7378ed3269d904a6c9e0b942d674fb76b749ce6f
hoangdaidp <65940270+hoangdaidp@users.noreply.github.com>
2020-12-30 18:18:22 +0700

    show dialog add category

--- app/src/main/java/com/example/noteapplication/Category_Activity.java
+++ app/src/main/java/com/example/noteapplication/Category_Activity.java
@@ -7,21 +7,23 @@ import android.view.View;
 import android.widget.Button;
 
 public class Category_Activity extends AppCompatActivity {
-    Button btn ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_);
-        btn  = findViewById(R.id.addCategory);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button  btnAddCategory  = findViewById(R.id.addCategory);
+        btnAddCategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openDialog();
             }
         });
+
     }
 
     public  void openDialog(){
-
+        Dialog_Add_Category dialogAddCategory = new Dialog_Add_Category();
+        dialogAddCategory.show(getSupportFragmentManager(),"example dialog");
     }
 }
---
+++ app/src/main/java/com/example/noteapplication/Dialog_Add_Category.java
@@ -0,0 +1,36 @@
+package com.example.noteapplication;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class Dialog_Add_Category extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog( Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog_add_category,null);
+        builder.setView(view)
+                .setTitle("Category From").setNegativeButton("close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setPositiveButton("add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        view.findViewById(R.id.)
+        return builder.create();
+    }
+}

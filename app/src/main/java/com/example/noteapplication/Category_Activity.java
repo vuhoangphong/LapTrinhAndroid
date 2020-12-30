8fe9fba42950810f806154fe0de76bad87d164ad
hoangdaidp <65940270+hoangdaidp@users.noreply.github.com>
2020-12-30 16:59:22 +0700

    add dialog category

--- app/src/main/java/com/example/noteapplication/Category_Activity.java
+++ app/src/main/java/com/example/noteapplication/Category_Activity.java
@@ -3,12 +3,25 @@ package com.example.noteapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Category_Activity extends AppCompatActivity {
-
+    Button btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_);
+        btn  = findViewById(R.id.addCategory);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public  void openDialog(){
+
     }
 }

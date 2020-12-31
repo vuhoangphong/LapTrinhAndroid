5273ecef3dd138de6a8283279bead687b19a60bc
hoangdaidp <65940270+hoangdaidp@users.noreply.github.com>
2020-12-31 11:44:08 +0700

    update category
    add layout row_category and change button plus

--- app/src/main/java/com/example/noteapplication/Category_Activity.java
+++ app/src/main/java/com/example/noteapplication/Category_Activity.java
@@ -1,20 +1,34 @@
 package com.example.noteapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Category_Activity extends AppCompatActivity implements Dialog_Add_Category.dialog_Add_Category_Listener {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category_Activity extends AppCompatActivity implements Dialog_Add_Category.dialog_Add_Category_Listener {
+    private List<Category> listcategory = new ArrayList<Category>();
+    private CategoryAdapter adapter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_);
-        Button  btnAddCategory  = findViewById(R.id.addCategory);
+        FloatingActionButton  btnAddCategory  = findViewById(R.id.addCategory);
         btnAddCategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,8 +45,38 @@ public class Category_Activity extends AppCompatActivity implements Dialog_Add_C
 
     @Override
     public void applyAdd(String category, String date) {
-        Toast.makeText( getApplicationContext(),category+" "+date,Toast.LENGTH_SHORT);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(category);
+        Category c = new Category();
+        c.setName(category);
+        c.setCreatedate(date);
+        listcategory.add(c);
+        ListView listView = findViewById(R.id.list_item);
+        adapter = new CategoryAdapter() ;
+       listView.setAdapter(adapter);
     }
+
+    public class CategoryAdapter extends ArrayAdapter<Category>{
+        public CategoryAdapter(Context context ,int textViewResourceId){
+            super(context,textViewResourceId);
+        }
+        public CategoryAdapter(){
+            super(Category_Activity.this, android.R.layout.simple_list_item_1,listcategory);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View row = convertView;
+            if(row == null){
+                LayoutInflater inflater = getLayoutInflater();
+                row = inflater.inflate(R.layout.row_category,null);
+            }
+            Category c = listcategory.get(position);
+            ((TextView)row.findViewById(R.id.name)).setText(c.getName());
+            ((TextView)row.findViewById(R.id.date)).setText(c.getCreatedate());
+            return row;
+        }
+    }
+
+
+
 }

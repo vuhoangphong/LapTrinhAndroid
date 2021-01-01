6e32f410d6ae0bc64c2c7de42cf14b2a19935ccc
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2021-01-01 14:26:51 +0700

    Update Login

--- app/src/main/java/com/example/noteapplication/Login.java
+++ app/src/main/java/com/example/noteapplication/Login.java
@@ -2,7 +2,12 @@ package com.example.noteapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText etUserName = (EditText) findViewById(R.id.editTextUserName);
+        EditText etPass = (EditText) findViewById(R.id.editTextPassWord);
+
+        Button button = (Button) findViewById(R.id.buttonLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                if(etUserName.getText().toString().length()>0&&etPass.getText().toString().length()>0){
+
+                    if(etUserName.getText().toString().equals("admin")&&etPass.getText().toString().equals("admin")){
+                        Intent intent = new Intent(Login.this,MainActivity.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(Login.this,R.string.incorrectUserPass,Toast.LENGTH_LONG).show();
+                    }
+                }else {
+                    Toast.makeText(Login.this,R.string.inputUserNamePass,Toast.LENGTH_LONG).show();
+            }
+        }});
+
+        }
     }
-}
+
--- app/src/main/java/com/example/noteapplication/MainActivity.java
+++ app/src/main/java/com/example/noteapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.noteapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -22,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 

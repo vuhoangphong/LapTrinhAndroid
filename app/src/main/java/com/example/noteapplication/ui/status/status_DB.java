dfebfa2d4fe6f5fb579a99e3acc7d7690d64ee01
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2021-01-03 12:05:02 +0700

    Update status_DB.java

--- app/src/main/java/com/example/noteapplication/ui/status/status_DB.java
+++ app/src/main/java/com/example/noteapplication/ui/status/status_DB.java
@@ -7,10 +7,13 @@ import android.database.sqlite.SQLiteDatabase;
 
 
 import com.example.noteapplication.DBHelper;
+import com.example.noteapplication.Login;
 
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.noteapplication.Login.AccInfo;
 
 public class status_DB extends DBHelper {
 
@@ -23,6 +26,7 @@ public class status_DB extends DBHelper {
         ContentValues contentValues = new ContentValues();
         contentValues.put("NameStatus",status.getName());
         contentValues.put("DateSta",status.getDatetime());
+        contentValues.put("IDAcc", AccInfo.getId());
         long inserted = db.insert(TABLE_STATUS,null,contentValues);
         if(inserted == -1 )
             return false;
@@ -34,7 +38,7 @@ public class status_DB extends DBHelper {
 
     public List<StatusViewModel> GetListStatus (){
         List<StatusViewModel> lstStatus = new ArrayList<StatusViewModel>();
-        String queryString = "SELECT * FROM " + TABLE_STATUS;
+        String queryString = "SELECT * FROM " + TABLE_STATUS + " Where IDAcc = " + AccInfo.getId() ;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(queryString,null);
         if(cursor.moveToFirst()){

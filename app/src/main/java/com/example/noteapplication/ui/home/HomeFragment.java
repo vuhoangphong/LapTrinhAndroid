bf955af5ada2726baef7f078516787978f1051f8
hoangdaidp <65940270+hoangdaidp@users.noreply.github.com>
2021-01-03 11:01:43 +0700

    dashboard example

--- app/src/main/java/com/example/noteapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/noteapplication/ui/home/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.example.noteapplication.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,24 +14,101 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.noteapplication.R;
 
-public class HomeFragment extends Fragment {
+
+import com.example.noteapplication.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements OnChartValueSelectedListener {
 
     private HomeViewModel homeViewModel;
-
+     private PieChart mChart;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+       // final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            //    textView.setText(s);
             }
         });
+
+
+
+        mChart = (PieChart) root.findViewById(R.id.piechart);
+        mChart.setRotationEnabled(true);
+        mChart.setDescription(new Description());
+        mChart.setHoleRadius(35f);
+        mChart.setTransparentCircleAlpha(0);
+        mChart.setCenterText("PieChart");
+        mChart.setCenterTextSize(10);
+        mChart.setDrawEntryLabels(true);
+
+        addDataSet(mChart);
+
+        mChart.setOnChartValueSelectedListener(this);
+
         return root;
+    }
+
+    private static void addDataSet(PieChart pieChart) {
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+        float[] yData = { 25, 40, 70 };
+        String[] xData = { "January", "February", "January" };
+
+        for (int i = 0; i < yData.length;i++){
+            yEntrys.add(new PieEntry(yData[i],i));
+        }
+        for (int i = 0; i < xData.length;i++){
+            xEntrys.add(xData[i]);
+        }
+
+        PieDataSet pieDataSet=new PieDataSet(yEntrys,"Employee Sales");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        ArrayList<Integer> colors=new ArrayList<>();
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+
+        pieDataSet.setColors(colors);
+
+        Legend legend=pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        PieData pieData=new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Toast.makeText(getContext(), "Value: "
+                + e.getY()
+                + ", index: "
+                + h.getX()
+                + ", DataSet index: "
+                + h.getDataSetIndex(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected() {
+
     }
 }

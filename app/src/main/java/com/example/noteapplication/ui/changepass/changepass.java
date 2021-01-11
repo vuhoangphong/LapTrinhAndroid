11fd1016327c2067552063404b28f03b70769281
nguyentanthai1905 <73575611+nguyentanthai1905@users.noreply.github.com>
2021-01-11 18:42:13 +0700

    update change password
    update change password

--- app/src/main/java/com/example/noteapplication/ui/changepass/changepass.java
+++ app/src/main/java/com/example/noteapplication/ui/changepass/changepass.java
@@ -50,32 +50,37 @@ public class changepass extends Fragment {
                 String pass_old = edOld_Pass.getText().toString();
                 String pass_new = edNew_Pass.getText().toString();
                 String pass_new_agian = edAgain_Pass.getText().toString();
-                if(pass_new.equals("") && pass_new_agian.equals("") && pass_old.equals("")) {
-                    Toast.makeText(changepass.this.getContext()," Error, please enter value ",Toast.LENGTH_SHORT).show();
-                }else if(pass_new_agian.equals("") && pass_new.equals("")){
-                    Toast.makeText(changepass.this.getContext()," Error, please enter new password and confirm password ",Toast.LENGTH_SHORT).show();
-                }else if(pass_old.equals("") && pass_new.equals("")){
-                    Toast.makeText(changepass.this.getContext()," Error, please enter old password and new password ",Toast.LENGTH_SHORT).show();
-                }else if(pass_old.equals("") && pass_new_agian.equals("")){
-                    Toast.makeText(changepass.this.getContext()," Error, please enter old password and confirm password ",Toast.LENGTH_SHORT).show();
-                }else if(pass_new.equals("")) {
-                    Toast.makeText(changepass.this.getContext(), " Error, please enter new password ", Toast.LENGTH_SHORT).show();
-                }else if(pass_new_agian.equals("")) {
-                    Toast.makeText(changepass.this.getContext(), " Error, please enter new confirm password ", Toast.LENGTH_SHORT).show();
-                }else if(pass_old.equals("")) {
-                    Toast.makeText(changepass.this.getContext(), " Error, please enter old password ", Toast.LENGTH_SHORT).show();
-                }else {
-                    if(pass_new.equals(pass_new_agian)) {
-                        ChangePassOJ changePassOJ = new ChangePassOJ(AccInfo.getId(), AccInfo.getFirstName(), AccInfo.getLastName(), AccInfo.getEmail(), pass_new);
-                        boolean update = changePass_db.updatePass(changePassOJ);
-                        if (update == true) {
-                            Intent intent = new Intent(changepass.this.getContext(), MainActivity.class);
-                            startActivity(intent);
-                            Toast.makeText(changepass.this.getContext(), " update password ", Toast.LENGTH_SHORT).show();
-                        } else
-                            Toast.makeText(changepass.this.getContext(), " update error password ", Toast.LENGTH_SHORT).show();
+                if(pass_old.equals(AccInfo.getPassWord())){
+                    if(pass_new.equals("") && pass_new_agian.equals("") && pass_old.equals("")) {
+                        Toast.makeText(changepass.this.getContext()," Error, please enter value ",Toast.LENGTH_SHORT).show();
+                    }else if(pass_new_agian.equals("") && pass_new.equals("")){
+                        Toast.makeText(changepass.this.getContext()," Error, please enter new password and confirm password ",Toast.LENGTH_SHORT).show();
+                    }else if(pass_old.equals("") && pass_new.equals("")){
+                        Toast.makeText(changepass.this.getContext()," Error, please enter old password and new password ",Toast.LENGTH_SHORT).show();
+                    }else if(pass_old.equals("") && pass_new_agian.equals("")){
+                        Toast.makeText(changepass.this.getContext()," Error, please enter old password and confirm password ",Toast.LENGTH_SHORT).show();
+                    }else if(pass_new.equals("")) {
+                        Toast.makeText(changepass.this.getContext(), " Error, please enter new password ", Toast.LENGTH_SHORT).show();
+                    }else if(pass_new_agian.equals("")) {
+                        Toast.makeText(changepass.this.getContext(), " Error, please enter new confirm password ", Toast.LENGTH_SHORT).show();
+                    }else if(pass_old.equals("")) {
+                        Toast.makeText(changepass.this.getContext(), " Error, please enter old password ", Toast.LENGTH_SHORT).show();
+                    }else {
+                        if(pass_new.equals(pass_new_agian)) {
+                            ChangePassOJ changePassOJ = new ChangePassOJ(AccInfo.getId(), AccInfo.getFirstName(), AccInfo.getLastName(), AccInfo.getEmail(), pass_new);
+                            boolean update = changePass_db.updatePass(changePassOJ);
+                            if (update == true) {
+                                Intent intent = new Intent(changepass.this.getContext(), MainActivity.class);
+                                startActivity(intent);
+                                Toast.makeText(changepass.this.getContext(), " update password ", Toast.LENGTH_SHORT).show();
+                            } else
+                                Toast.makeText(changepass.this.getContext(), " update error password ", Toast.LENGTH_SHORT).show();
+                        }
                     }
+                }else {
+                    Toast.makeText(changepass.this.getContext(), " password not match ", Toast.LENGTH_SHORT).show();
                 }
+
                 /*if(pass_new.equals(pass_new_agian))
                 {
                     ChangePassOJ changePassOJ = new ChangePassOJ(AccInfo.getId(),AccInfo.getFirstName(),AccInfo.getLastName(),AccInfo.getEmail(),pass_new);
